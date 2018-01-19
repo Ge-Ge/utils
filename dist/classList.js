cc458aef7f22c2474bf85567345d7b7b801d1a8d
ge-ge <WXJ907468952@outlook.com>
2018-01-19 13:55:23 +0800

    修改：     .gitignore 	新文件：   dist/classList.js 	新文件：   dist/index.js

---
+++ dist/classList.js
@@ -0,0 +1,42 @@
+var ClassList = /** @class */ (function () {
+    function ClassList(ele) {
+        this.ele = ele;
+    }
+    ClassList.prototype.hasClass = function (className) {
+        if (!className)
+            return false;
+        if (this.ele.classList) {
+            return this.ele.classList.contains(className);
+        }
+        else {
+            return new RegExp('(\\s|^)' + className + '(\\s|$)').test(this.ele.className);
+        }
+    };
+    ClassList.prototype.addClass = function (className) {
+        if (!className)
+            return this;
+        if (this.ele.classList) {
+            this.ele.classList.add(className);
+        }
+        else {
+            if (this.hasClass(className)) {
+                this.ele.className += ' ' + className;
+            }
+        }
+        return this;
+    };
+    ClassList.prototype.removeClass = function (className) {
+        if (this.ele.classList) {
+            this.ele.classList.remove(className);
+        }
+        else {
+            if (this.hasClass(className)) {
+                var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+                this.ele.className = this.ele.className.replace(reg, '');
+            }
+        }
+        return this;
+    };
+    return ClassList;
+}());
+export default ClassList;
